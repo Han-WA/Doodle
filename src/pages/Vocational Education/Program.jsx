5576a323888a221f18087996f81ae641eb80d402
thawzinmoemyint <thawzinmoemyint@thompsons-MacBook-Pro.local>
2024-05-03 01:25:22 +0700

    add vocational educational section

--- src/pages/Vocational Education/Program.jsx
+++ src/pages/Vocational Education/Program.jsx
@@ -8,7 +8,7 @@ export const Program = () => {
       <div className="container">
         <div className="heading py-12 text-center w-2/3 m-auto md:w-full">
           <h1 className="text-3xl font-semibold text-black">
-            <span className="border-b-2 border-black pb-2">Overview</span>
+            <span className="border-b-2 border-black pb-2">Programs</span>
             </h1>
         </div>
 
